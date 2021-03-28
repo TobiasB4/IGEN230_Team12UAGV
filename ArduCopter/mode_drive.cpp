62d907bb0441c3b3e9d6e384d4687efe259f8d6a
TobiasB4 <Tobi.Bobb.TB@gmail.com>
2021-03-28 13:22:39 -0700

    Updating drive mode and added disable/enable function to ap_motormatrix. We will have to change drive mode after because some of the stuff I in needs to go into class

--- ArduCopter/mode_drive.cpp
+++ ArduCopter/mode_drive.cpp
@@ -1,11 +1,53 @@
 #include "Copter.h"
 
+#define ThrottleOut 0
+#define SteeringOut 1
+
 bool ModeDrive::init(bool ignore_checks)
 {
+    //set_range() for SRV_CHANNEL to 1 for throttle calcs
+    //disable all motors except for our servos steering and throttle
     return true;
 }
 
 void ModeDrive::run()
 {
+    //Ask Daniel to put in public
+    //
+    float driveModeOutputs[] = {0, 0};
+
+    ModeDrive::get_pilot_input(driveMod)
+
+        if (!motors->armed())
+    {
+        // Motors should be Stopped
+        motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::SHUT_DOWN);
+    }
     // find the code under Rover/mode_steering.cpp
+}
+
+//add this prototype to class def
+void ModeDrive::get_pilot_input(float driveModeOutputs[])
+{
+
+    if ("Something bad happens or no rc input")
+    {
+        //tell Daniel to add to the class and declare zero on mode drive init
+        driveModeOutputs[ThrottleOut] = 0;
+        driveModeOutputs[SteeringOut] = 0;
+        return;
+    }
+
+    //Some PWM signal between 900 and 2100
+    //set_output_norm() found in SRV_CHANNEL
+    //force it to between 0 and 1
+    driveModeOutputs[SteeringOut] = channel_roll->get_control_in();
+
+    // some value between 0 and 1
+    //pwm_from_range found in SRV_CHANNEL
+    //becomes throttle actuator value
+    //function(actuator, value)
+    driveModeOutputs[ThrottleOut] = Mode::get_pilot_desired_throttle();
+
+    AP_MotorsMulticopter::set_actuator_with_slew(AP_MotorsMulticopter::_actuator)
 }
