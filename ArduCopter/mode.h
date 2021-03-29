e3799ac3534d216f71ce183c5ecda257ecdde06f
TobiasB4 <Tobi.Bobb.TB@gmail.com>
2021-03-29 13:52:20 -0700

    Changed array name in mode.h

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -11,33 +11,32 @@ class Mode
 
 public:
     // Auto Pilot Modes enumeration
-Class
-    enum class Number : uint8_t {
-        STABILIZE =     0,  // manual airframe angle with manual throttle
-        ACRO =          1,  // manual body-frame angular rate with manual throttle
-        ALT_HOLD =      2,  // manual airframe angle with automatic throttle
-        AUTO =          3,  // fully automatic waypoint control using mission commands
-        GUIDED =        4,  // fully automatic fly to coordinate or fly at velocity/direction using GCS immediate commands
-        LOITER =        5,  // automatic horizontal acceleration with automatic throttle
-        RTL =           6,  // automatic return to launching point
-        CIRCLE =        7,  // automatic circular flight with automatic throttle
-        LAND =          9,  // automatic landing with horizontal position control
-        DRIFT =        11,  // semi-autonomous position, yaw and throttle control
-        SPORT =        13,  // manual earth-frame angular rate control with manual throttle
-        FLIP =         14,  // automatically flip the vehicle on the roll axis
-        AUTOTUNE =     15,  // automatically tune the vehicle's roll and pitch gains
-        POSHOLD =      16,  // automatic position hold with manual override, with automatic throttle
-        BRAKE =        17,  // full-brake using inertial/GPS system, no pilot input
-        THROW =        18,  // throw to launch mode using inertial/GPS system, no pilot input
-        AVOID_ADSB =   19,  // automatic avoidance of obstacles in the macro scale - e.g. full-sized aircraft
-        GUIDED_NOGPS = 20,  // guided mode but only accepts attitude and altitude
-        SMART_RTL =    21,  // SMART_RTL returns to home by retracing its steps
-        FLOWHOLD  =    22,  // FLOWHOLD holds position with optical flow without rangefinder
-        FOLLOW    =    23,  // follow attempts to follow another vehicle or ground station
-        ZIGZAG    =    24,  // ZIGZAG mode is able to fly in a zigzag manner with predefined point A and point B
-        SYSTEMID  =    25,  // System ID mode produces automated system identification signals in the controllers
-        AUTOROTATE =   26,  // Autonomous autorotation
-        DRIVE =        27,  // Drive mode
+    Class enum class Number : uint8_t {
+        STABILIZE = 0,     // manual airframe angle with manual throttle
+        ACRO = 1,          // manual body-frame angular rate with manual throttle
+        ALT_HOLD = 2,      // manual airframe angle with automatic throttle
+        AUTO = 3,          // fully automatic waypoint control using mission commands
+        GUIDED = 4,        // fully automatic fly to coordinate or fly at velocity/direction using GCS immediate commands
+        LOITER = 5,        // automatic horizontal acceleration with automatic throttle
+        RTL = 6,           // automatic return to launching point
+        CIRCLE = 7,        // automatic circular flight with automatic throttle
+        LAND = 9,          // automatic landing with horizontal position control
+        DRIFT = 11,        // semi-autonomous position, yaw and throttle control
+        SPORT = 13,        // manual earth-frame angular rate control with manual throttle
+        FLIP = 14,         // automatically flip the vehicle on the roll axis
+        AUTOTUNE = 15,     // automatically tune the vehicle's roll and pitch gains
+        POSHOLD = 16,      // automatic position hold with manual override, with automatic throttle
+        BRAKE = 17,        // full-brake using inertial/GPS system, no pilot input
+        THROW = 18,        // throw to launch mode using inertial/GPS system, no pilot input
+        AVOID_ADSB = 19,   // automatic avoidance of obstacles in the macro scale - e.g. full-sized aircraft
+        GUIDED_NOGPS = 20, // guided mode but only accepts attitude and altitude
+        SMART_RTL = 21,    // SMART_RTL returns to home by retracing its steps
+        FLOWHOLD = 22,     // FLOWHOLD holds position with optical flow without rangefinder
+        FOLLOW = 23,       // follow attempts to follow another vehicle or ground station
+        ZIGZAG = 24,       // ZIGZAG mode is able to fly in a zigzag manner with predefined point A and point B
+        SYSTEMID = 25,     // System ID mode produces automated system identification signals in the controllers
+        AUTOROTATE = 26,   // Autonomous autorotation
+        DRIVE = 27,        // Drive mode
     };
 
     // constructor
@@ -1259,37 +1258,38 @@ protected:
 private:
 };
 
-class ModeDrive : public Mode {
+class ModeDrive : public Mode
+{
 
 public:
-  using Mode::Mode;
-  Number mode_number() const override{return Number::DRIVE;}
-  float signal_output [] = {0,0};
+    using Mode::Mode;
+    Number mode_number() const override { return Number::DRIVE; }
+    float driveModeOutputs[] = {0, 0};
 
-	// methods that affect movement of the vehicle in this mode
-  bool init(bool ignore_checks) override;
-	virtual void run() override;
-  void exit();
-  void get_pilot_input();
+    // methods that affect movement of the vehicle in this mode
+    bool init(bool ignore_checks) override;
+    virtual void run() override;
+    void exit();
+    void get_pilot_input();
 
-	// attributes for mavlink system status reporting
-	bool has_manual_input() const override { return true; }
-	bool attitude_stabilized() const override { return false; }
+    // attributes for mavlink system status reporting
+    bool has_manual_input() const override { return true; }
+    bool attitude_stabilized() const override { return false; }
 
-	// steering requires velocity but not position
-	bool requires_position() const override { return false; }
-	bool requires_velocity() const override { return true; }
-  bool has_manual_throttle() const override { return true; }
-  bool is_autopilot() const override { return false; }
-  bool allows_arming(bool from_gcs) const override { return true; };
-  
+    // steering requires velocity but not position
+    bool requires_position() const override { return false; }
+    bool requires_velocity() const override { return true; }
+    bool has_manual_throttle() const override { return true; }
+    bool is_autopilot() const override { return false; }
+    bool allows_arming(bool from_gcs) const override { return true; };
+
 protected:
-  const char *name() const override { return "DRIVEMODE"; }
-	const char *name4() const override { return "DRIV"; }
-    
-private: 
-     const int ThrottleOut = 0; 
-     const int SteeringOut = 1;
+    const char *name() const override { return "DRIVEMODE"; }
+    const char *name4() const override { return "DRIV"; }
+
+private:
+    const int ThrottleOut = 0;
+    const int SteeringOut = 1;
 };
 
 #if FRAME_CONFIG == HELI_FRAME
